fad9675ded257ed43f8ca132b2eeaa3644ec91f5
Reda Bagha <baghareda@gmail.com>
2025-05-26 16:08:24 +0100

    age calculator

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,14 +1,2 @@
-user_input = input("How old are you?\n")
-
-is_number = True
-for char in user_input:
-    if char < '0' or char > '9':
-        is_number = False
-        break
-
-if is_number:
-    age = int(user_input)
-    print("In 2050, you will be", age + 27, "years old.")
-else:
-    print("That's not a valid age.")
-
+age = int(input("How old are you?\n"))
+print ("In 2050, you will be", age + 27, "years old.")

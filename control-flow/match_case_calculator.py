42a0b960c17847c10c959a1a1d716a3ceab96c8b
Reda Bagha <baghareda@gmail.com>
2025-05-29 18:54:15 +0100

    match case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,7 +1,7 @@
 num1 =float(input("Enter the first number:"))
 num2 =float(input("Enter the second number:"))
-opr =input("Choose the operation (+, -, *, /):")
-match opr:
+operation =input("Choose the operation (+, -, *, /):")
+match operation:
  case   "*":
     print("The result is: ", num1 * num2)
  case   "+":

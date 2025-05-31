76a8b0ff0bb6a53c31cc25e91c01acfacd160d29
Reda Bagha <baghareda@gmail.com>
2025-05-31 17:10:07 +0100

    multi table

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,6 +2,6 @@ number = int(input("Enter a number to see its multiplication table: "))
 
 if number >= 0 and number <= 10:
     for m in range(1, 11):
-        print (f"{number} X {m} = {number * m}")
+        print (f"{number} * {m} = {number * m}")
 else:
         print ("enter a valid number")

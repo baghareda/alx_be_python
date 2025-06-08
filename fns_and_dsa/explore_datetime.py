7048a0342c2436890798c767e5803c4c423c116e
Reda Bagha <baghareda@gmail.com>
2025-06-08 20:39:23 +0100

    datetime

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,14 @@
+import datetime
+
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    print(current_date)
+    return current_date
+
+def calculate_future_date(now):
+    calculate_date = int(input("enter how many day you want to add: "))
+    future_date = now + datetime.timedelta(days=calculate_date)
+    print(future_date)
+
+now = display_current_datetime()
+calculate_future_date(now)

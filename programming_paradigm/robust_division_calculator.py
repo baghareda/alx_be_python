20839290b81ebd93cf59d2f34b140211fcab2619
Reda Bagha <baghareda@gmail.com>
2025-06-12 16:21:55 +0100

    rob div cal

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -4,7 +4,7 @@ def safe_divide(numerator, denominator):
         denom = float(denominator)
         res = num / denom
         return f"the redult of the division is: {res}"
-        except ZeroDivisionError:
-            return "Error: Cannot divide by zero."
-        except ValueError:
-            return "Error: Please enter numeric values only."
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Please enter numeric values only."

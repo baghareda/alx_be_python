a0bf47f28782533c6251f35ce834b062df049fa1
Reda Bagha <baghareda@gmail.com>
2025-06-13 01:55:05 +0100

    test unittest

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,7 +12,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(1000, 0), 1000)
         self.assertEqual(self.calc.add(2, 2), 4)
         
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(2, 3), -1)
         self.assertEqual(self.calc.subtract(0, 0), 0)
         self.assertEqual(self.calc.subtract(1000, 0), 1000)

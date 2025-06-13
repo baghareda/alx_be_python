9cc204a8d4b29f335312d69c37ba88537f630802
Reda Bagha <baghareda@gmail.com>
2025-06-13 01:54:00 +0100

    test unittest

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,7 +6,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
         
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(0, 0), 0)
         self.assertEqual(self.calc.add(1000, 0), 1000)
@@ -18,13 +18,13 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(1000, 0), 1000)
         self.assertEqual(self.calc.subtract(2, 2), 0)
         
-    def test_subtract(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(0, 0), 0)
         self.assertEqual(self.calc.multiply(1000, 0), 0)
         self.assertEqual(self.calc.multiply(-2, -2), 4)
         
-    def test_subtract(self):
+    def test_dividing(self):
         self.assertEqual(self.calc.divide(2, 1), 2)
         self.assertEqual(self.calc.divide(0, 1), 0)
         self.assertEqual(self.calc.divide(1000, 0), None)

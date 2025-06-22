bb66b0a4a1e0dfc2d12f1626603eb412924a6f15
Reda Bagha <baghareda@gmail.com>
2025-06-23 00:24:46 +0100

    oop book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -13,6 +13,6 @@ class Book:
         return f"Book('{self.title}', '{self.author}', {self.year})"
         
     def __del__(self):
-        print(f"Deleting: {self.title}")
+        print(f"Deleting {self.title}")
         
     
